42915d19ce654ac868df5c178df8b53c84253510
christianlowe <christglowe@gmail.com>
2018-07-13 22:15:04 -0600

    Added more methods to the fraction class

--- DEV277x/Fraction.java
+++ DEV277x/Fraction.java
@@ -3,6 +3,15 @@ package DEV277x;
 public class Fraction {
     private int numerator;
     private  int denominator;
+
+    public Fraction(Integer numerator) {
+        this(numerator, 1);
+    }
+
+    public Fraction() {
+        this(0, 1);
+    }
+
     public Fraction(int numerator, int denominator) {
         if (denominator == 0 ) {
             throw new IllegalArgumentException();
@@ -14,7 +23,32 @@ public class Fraction {
         this.denominator = denominator;
 
     }
-    public String getFraction() {
+
+    public String toString() {
        return "" + this.numerator + "/" +  this.denominator;
     }
+
+    public double toDouble() {
+        return (numerator / denominator);
+    }
+
+    public int getNumerator() {
+        return this.numerator;
+    }
+
+    public int getDenominator() {
+      return this.denominator;
+    }
+
+    public double add(double fraction) {
+        return (this.numerator / this.denominator) + fraction;
+    }
+
+    public double subtract(double fraction) {
+        return (this.numerator / this.denominator) + fraction;
+    }
+
+    
+
+
 }
